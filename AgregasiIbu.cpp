b4a4a8cfdd15bba243c52510b5b55a95352a31b6
dfamrln <mhdffa1@gmail.com>
2026-05-11 10:42:14 +0700

    looping

--- AgregasiIbu.cpp
+++ AgregasiIbu.cpp
@@ -24,7 +24,10 @@ void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
 void ibu::cetakAnak() {
-    cout <<
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+    }
 }
 
 

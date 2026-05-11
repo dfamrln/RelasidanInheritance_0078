5b627acbea3336dad3248350d0f2a709738ba168
dfamrln <mhdffa1@gmail.com>
2026-05-11 10:37:22 +0700

    destruktor ibu

--- AgregasiIbu.cpp
+++ AgregasiIbu.cpp
@@ -14,6 +14,9 @@ public:
     ibu(string pNama) :nama(pNama) {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
+    ~ibu(){
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
 }
 
 

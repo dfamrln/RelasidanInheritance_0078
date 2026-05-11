73ed30df4147647feff9f554b653869a7a0988b3
dfamrln <mhdffa1@gmail.com>
2026-05-11 10:28:11 +0700

    Perubahan

---
+++ AgregasiAnak.cpp
@@ -0,0 +1,18 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Anak {
+public:
+    string nama;
+    Anak(string pNama) : nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+    ~Anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
